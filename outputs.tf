fc7c3758492dd2baee6124883467c1bbf16776c9
TheIronRock95 <58424695+TheIronRock95@users.noreply.github.com>
2023-01-24 21:28:18 +0100

    fix: add name output

--- outputs.tf
+++ outputs.tf
@@ -3,3 +3,8 @@ output "azurerm_network_watcher_id" {
   sensitive   = false
   value       = try(azurerm_network_watcher.this.id)
 }
+output "azurerm_network_watcher_name" {
+  description = "The ID of the Network Watcher."
+  sensitive   = false
+  value       = try(azurerm_network_watcher.this.name)
+}
